10e3379facc3086b5b93c34d4f34872b9539667f
Jovie <jovietrotta@gmail.com>
2021-03-04 21:09:45 -0500

    second attempt to fix audio files

--- contact/contact.php
+++ contact/contact.php
@@ -18,14 +18,14 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'index.html';
+		window.location = 'contact-submission.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Message failed. Please, send an email to matt.lombardi.matt@gmail.com');
-		window.location = 'index.html';
+		window.location = 'contact-submission.html';
 	</script>
 <?php
 }
